f45521263366eeec37dae87649cb01560b263879
siva <sivasebi4545@gmail.com>
2023-07-18 00:57:54 +0530

    addfibonacci

---
+++ main.js
@@ -0,0 +1,12 @@
+for(let i=1;i<=10;i++){
+  for(let k=1;k<=(10-i);k++){
+       document.write("&nbsp");
+	  
+}
+for(let j=1;j<=i;j++){
+     document.write("*");
+	 
+}
+    document.write("<br>");
+	
+}
